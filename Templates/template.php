5e44c15e2c9f82ecff55d9747aef13f0dc89b741
TJ <Cyr-@users.noreply.github.com>
2015-01-22 17:23:24 -0600

    Addition of CSS file and link to template

--- Templates/template.php
+++ Templates/template.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>The Template name</title>
-</head>
-<body>
-  <?php echo $data; ?>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>The Template name</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+  </head>
+  <body>
+    <?php echo $data; ?>
+  </body>
 </html>
