c67b18baedcec07de7ae0eb56b2d5931d8efc065
TJ <Cyr-@users.noreply.github.com>
2015-02-04 16:57:16 -0600

    Set up model to use pokeapi

--- Models/model.php
+++ Models/model.php
@@ -2,25 +2,50 @@
 
 class Model
 {
-  public $title;
-  public $content;
-  public $created_at;
+  public $name;
+  public $height;
+  public $weight;
+  public $species;
+  public $hp;
+  public $attack;
+  public $defence;
+  public $special_attack;
+  public $special_defence;
+  public $speed;
 
   public function __construct() {
-    $this->template    = 'Templates/template.php';
+    $this->template = 'Templates/template.php';
+    $pokemon_json   = $this->getPokemonAPIValues(25);
 
-    $blog              = $this->get_blog_post(1);
-
-    $this->title       = $blog['title'];
-    $this->content     = $blog['content'];
-    $this->created_at  = $blog['created_at'];
+    $this->name   = $pokemon_json->name;
+    $this->height = $pokemon_json->height;
+    $this->weight = $pokemon_json->weight;
   }
 
-  private function get_blog_post($id) {
-    require 'connect.php';
-    $blog_query = "SELECT * FROM blog_posts WHERE id = '{$id}'";
-    $blog_data  = $db->query($blog_query);
+  private function getPokemonAPIValues($id) {
+    $cache_file = "./Cache/Pokemon/{$id}.json";
+    $url        = "http://pokeapi.co/api/v1/pokemon/{$id}/";
 
-    return $blog_data->fetch_assoc();
+    if (is_file($cache_file)) {
+      if ($this->timePassed($cache_file)) {
+        $this->cacheFile($url, $cache_file);
+      }
+    }
+    else {
+      $this->cacheFile($url, $cache_file);
+    }
+
+    return json_decode(file_get_contents($cache_file));
+  }
+
+  private function timePassed($file) {
+    $time_passed = (strtotime("now") - filemtime($file));
+
+    return $time_passed > 60*60*24*30;
+  }
+
+  private function cacheFile($url, $file) {
+    $json = file_get_contents($url);
+    file_put_contents($file, $json);
   }
 }
