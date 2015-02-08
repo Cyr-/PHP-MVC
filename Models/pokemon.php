d03231f22944b548419100d0615f0983fbc73376
TJ <Cyr-@users.noreply.github.com>
2015-02-08 12:49:09 -0600

    Modifications to model to get sprites

--- Models/pokemon.php
+++ Models/pokemon.php
@@ -12,15 +12,17 @@ class Pokemon
   public $special_attack;
   public $special_defence;
   public $speed;
+  public $sprite_url;
 
   public function __construct($id) {
     $this->id     = $id;
-    $pokemon_json = $this->getPokemonAPIValues($this->id);
+    $pokemon_json = $this->getPokemonAPIValues();
+    $sprite_json  = $this->getPokemonSprite($pokemon_json);
 
     $this->name    = $pokemon_json->name;
     $this->species = ucwords($pokemon_json->species);
-    $this->height  = $pokemon_json->height;
-    $this->weight  = $pokemon_json->weight;
+    $this->height  = ($pokemon_json->height)/10;
+    $this->weight  = ($pokemon_json->weight)/10;
 
     $this->hp              = $pokemon_json->hp;
     $this->attack          = $pokemon_json->attack;
@@ -28,11 +30,29 @@ class Pokemon
     $this->special_attack  = $pokemon_json->sp_atk;
     $this->special_defence = $pokemon_json->sp_def;
     $this->speed           = $pokemon_json->speed;
+
+    $this->sprite_url = "http://pokeapi.co{$sprite_json->image}";
   }
 
-  private function getPokemonAPIValues($id) {
-    $cache_file = "./Cache/Pokemon/{$id}.json";
-    $url        = "http://pokeapi.co/api/v1/pokemon/{$id}/";
+  private function getPokemonAPIValues() {
+    $cache_file = "./Cache/Pokemon/{$this->id}.json";
+    $url        = "http://pokeapi.co/api/v1/pokemon/{$this->id}/";
+
+    if (is_file($cache_file)) {
+      if ($this->timePassed($cache_file)) {
+        $this->cacheFile($url, $cache_file);
+      }
+    }
+    else {
+      $this->cacheFile($url, $cache_file);
+    }
+
+    return json_decode(file_get_contents($cache_file));
+  }
+
+  private function getPokemonSprite($pokemon_json) {
+    $cache_file = "./Cache/Sprite/{$this->id}.json";
+    $url        = "http://pokeapi.co{$pokemon_json->sprites[0]->resource_uri}";
 
     if (is_file($cache_file)) {
       if ($this->timePassed($cache_file)) {
