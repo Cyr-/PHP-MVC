7123b77bae00a8a34b3e7c4ac727833132eda57a
TJ <Cyr-@users.noreply.github.com>
2015-02-08 12:50:26 -0600

    Changes to page layout

--- Views/pokemon.php
+++ Views/pokemon.php
@@ -8,11 +8,7 @@ for($i = $gen_1_ids[0]; $i <= end($gen_1_ids); $i++) {
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP MVC</title>
-    <link rel="stylesheet" type="text/css" href="CSS/styles.css">
-  </head>
+  <?php require 'Partials/_head.html'; ?>
   <body>
     <div class="content">
       <?php require 'Partials/_header.html'; ?>
@@ -22,49 +18,44 @@ for($i = $gen_1_ids[0]; $i <= end($gen_1_ids); $i++) {
         <?php echo $option_list; ?>
       </select>
 
-      <h1>Name: <?php echo $pokemon->name; ?></h1>
-      <p>Species: <?php echo $pokemon->species; ?></p>
-      <p>Height: <?php echo $pokemon->height; ?> feet/inches</p>
-      <p>Weight: <?php echo $pokemon->weight; ?> pounds</p>
+      <div class="well">
+        <div class="sprite">
+          <img src="<?php echo $pokemon->sprite_url; ?>" alt="<?php echo $pokemon->name; ?>" />
+        </div>
+        <h1>Name: <?php echo $pokemon->name; ?></h1>
+        <p>Species: <?php echo $pokemon->species; ?></p>
+        <p>Height: <?php echo $pokemon->height; ?> meters</p>
+        <p>Weight: <?php echo $pokemon->weight; ?> kilograms</p>
+      </div>
 
-      <ul class="base_stats">
-        <li class="stat">
-          <strong>HP</strong>
-          <meter class="hp" max="255" value="<?php echo $pokemon->hp; ?>">
-            <strong>HP</strong>
-          </meter>
-        </li>
-        <li class="stat">
-          <strong>Attack</strong>
-          <meter class="attack" max="255" value="<?php echo $pokemon->attack; ?>">
-            <strong>Attack</strong>
-          </meter>
-        </li>
-        <li class="stat">
-          <strong>Defence</strong>
-          <meter class="defence" max="255" value="<?php echo $pokemon->defence; ?>">
-            <strong>Defence</strong>
-          </meter>
-        </li>
-        <li class="stat">
-          <strong>Special Attack</strong>
-          <meter class="special_attack" max="255" value="<?php echo $pokemon->special_attack; ?>">
-            <strong>Special Attack</strong>
-          </meter>
-        </li>
-        <li class="stat">
-          <strong>Special Defence</strong>
-          <meter class="special_defence" max="255" value="<?php echo $pokemon->special_defence; ?>">
-            <strong>Special Defence</strong>
-          </meter>
-        </li>
-        <li class="stat">
-          <strong>Speed</strong>
-          <meter class="speed" max="255" value="<?php echo $pokemon->speed; ?>">
-            <strong>Speed</strong>
-          </meter>
-        </li>
-      </ul>
+      <div class="well">
+        <ul class="base_stats">
+          <li class="stat">
+            <strong>HP: <?php echo $pokemon->hp; ?></strong>
+            <meter class="hp" max="255" value="<?php echo $pokemon->hp; ?>">
+          </li>
+          <li class="stat">
+            <strong>Attack: <?php echo $pokemon->attack; ?></strong>
+            <meter class="attack" max="255" value="<?php echo $pokemon->attack; ?>" />
+          </li>
+          <li class="stat">
+            <strong>Defence: <?php echo $pokemon->defence; ?></strong>
+            <meter class="defence" max="255" value="<?php echo $pokemon->defence; ?>" />
+          </li>
+          <li class="stat">
+            <strong>Special Attack: <?php echo $pokemon->special_attack; ?></strong>
+            <meter class="special_attack" max="255" value="<?php echo $pokemon->special_attack; ?>" />
+          </li>
+          <li class="stat">
+            <strong>Special Defence: <?php echo $pokemon->special_defence; ?></strong>
+            <meter class="special_defence" max="255" value="<?php echo $pokemon->special_defence; ?>" />
+          </li>
+          <li class="stat">
+            <strong>Speed: <?php echo $pokemon->speed; ?></strong>
+            <meter class="speed" max="255" value="<?php echo $pokemon->speed; ?>" />
+          </li>
+        </ul>
+      </div>
     </div>
   </body>
 </html>
